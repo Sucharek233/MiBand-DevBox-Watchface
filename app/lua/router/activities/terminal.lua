e82dd9093fff9f64e298f0173f6aeae5efb62c40
Sucharek233 <31042508+Sucharek233@users.noreply.github.com>
2026-09-10 01:07:04 +0200

    add args validator to terminal

--- app/lua/router/activities/terminal.lua
+++ app/lua/router/activities/terminal.lua
@@ -27,12 +27,14 @@ end
 function Terminal:run(request)
     local command = request.args.cmd
 
-    request.state = "running"
-    self.mailbox:writeMailbox(request)
-
     -- Wrapping the command in `sh -c` is needed to properly handle redirection and such
     local safeCmd = sanitizeShellCmd(command)
-    local shellCommand = string.format('sh -c "%s" > "%s"', safeCmd, self.paths.real .. "/" .. self.paths.outputFile)
+    local shellCommand = string.format(
+        'sh -c "%s" > "%s"',
+        safeCmd,
+        self.paths.real .. "/" .. self.paths.outputFile
+    )
+
     local _, reason, code = os.execute(shellCommand)
 
     request.state = MailboxStates.DONE
@@ -40,7 +42,35 @@ function Terminal:run(request)
     request.code = code
     request.out = self.paths.quickapp .. "/" .. self.paths.outputFile
     request.args = nil
+
     self.mailbox:writeMailbox(request)
+end
+
+function Terminal:handle(request)
+    print("[devbox]")
+    TablePrint(request)
+
+    local args = request.args or {}
+
+    local schema = {
+        required = {
+            cmd = "string"
+        }
+    }
+
+    local valid, err = ArgsValidator.validate(args, schema)
+
+    if not valid then
+        request.args = nil
+        request.state = MailboxStates.DONE
+        request.appState = MailboxStates.ERROR
+        request.res = err
+
+        self.mailbox:writeMailbox(request)
+        return
+    end
+
+    self:run(request)
 end
 
 function Terminal:clean()
